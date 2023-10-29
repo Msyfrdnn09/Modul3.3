0a531caa44d857daa7aae1c66130f115de12f24e
Msyfrdnn09 <149245538+Msyfrdnn09@users.noreply.github.com>
2023-10-29 21:17:39 +0700

    latihan

---
+++ LatihanPraktikum/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * kontolahhhhahhduafbagsrbg
+         */
+
+        // Kecepatan awal dalam m/s
+        double kecepatanAwal = 10.0;
+
+        // Kecepatan akhir dalam m/s
+        double kecepatanAkhir = 30.0;
+
+        // Waktu dalam detik
+        double waktu = 5.0;
+
+        // Menghitung percepatan
+        double perubahanKecepatan = kecepatanAkhir - kecepatanAwal;
+        double percepatan = perubahanKecepatan / waktu;
+
+        // Menampilkan hasil
+        System.out.println("Percepatan adalah: " + percepatan + " m/s^2");
+    }
+}
