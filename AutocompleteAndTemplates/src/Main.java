cb61ad62ff5e9ea0e44da48c2e32456a24052717
Msyfrdnn09 <149245538+Msyfrdnn09@users.noreply.github.com>
2023-10-30 11:16:08 +0700

    fitur git

--- AutocompleteAndTemplates/src/Main.java
+++ AutocompleteAndTemplates/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         /**
          * @param kecepatanAwal menunjukan angka awal
          * @param kecepatanAkhir menunjukan angka akhir
@@ -8,22 +7,11 @@ public class Main {
          * @param perubahanKecepatan menghitung perubahan kecepatan
          * @param percepatan menghitung percepatan
          */
-        // Kecepatan awal dalam m/s
         double kecepatanAwal = 10.0;
-
-        // Kecepatan akhir dalam m/s
         double kecepatanAkhir = 30.0;
-
-        // Waktu dalam detik
         double waktu = 5.0;
-
-        // Menghitung percepatan
         double perubahanKecepatan = kecepatanAkhir - kecepatanAwal;
         double percepatan = perubahanKecepatan / waktu;
-
-
-
-        // Menampilkan hasil
         System.out.println("Percepatan adalah: " + percepatan + " m/s^2");
     }
 }
